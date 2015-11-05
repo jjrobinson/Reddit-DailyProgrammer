f4749e9ec3fffcaae238806384ae3f3c076b822b
Jason Robinson <robinson.jason@gmail.com>
2015-11-04 17:27:46 -0700

    solution for challenge239Easy

--- src/challenge236Easy/TetrominoBag.java
+++ src/challenge236Easy/TetrominoBag.java
@@ -10,83 +10,86 @@ import java.util.Random;
  * 
  * Contrary to popular belief, the tetromino pieces you are given in a game of 
  * Tetris are not randomly selected. Instead, all seven pieces are placed into a
- * "bag." A piece is randomly removed from the bag and presented to the player 
- * until the bag is empty. When the bag is empty, it is refilled and the process
- * is repeated for any additional pieces that are needed.
- * 
- * In this way, it is assured that the player will never go too long without seeing
- *  a particular piece. It is possible for the player to receive two identical
- *  pieces in a row, but never three or more. Your task for today is to implement
- *  this system.
- * 
- * Input Description
- * None.
- * 
- * Output Description
- * 
- * Output a string signifying 50 tetromino pieces given to the player using the
- *  random bag system. This will be on a single line.
- *  
- * The pieces are as follows:
- * O
- * I
- * S
- * Z
- * L
- * J
- * T
- * Sample Inputs
- * 
- * None.
- * Sample Outputs
- * 
- * LJOZISTTLOSZIJOSTJZILLTZISJOOJSIZLTZISOJTLIOJLTSZO
- * OTJZSILILTZJOSOSIZTJLITZOJLSLZISTOJZTSIOJLZOSILJTS
- * ITJLZOSILJZSOTTJLOSIZIOLTZSJOLSJZITOZTLJISTLSZOIJO
- * Note
- * 
- * Although the output is semi-random, you can verify whether it is likely to be
- *  correct by making sure that pieces do not repeat within chunks of seven.
- * Credit
- * 
- * This challenge was developed by /u/chunes on /r/dailyprogrammer_ideas. If you 
- * have any challenge ideas please share them there and there's a chance we'll use 
- * them.
- * Bonus
- * 
- * Write a function that takes your output as input and verifies that it is a valid
- *  sequence of pieces.
- *   * @author JasonRobinson
+ "bag." A piece is randomly removed from the bag and presented to the player 
+ until the bag is empty. When the bag is empty, it is refilled and the process
+ is repeated for any additional pieces that are needed.
+ 
+ In this way, it is assured that the player will never go too long without seeing
+  a particular piece. It is possible for the player to receive two identical
+  pieces in a row, but never three or more. Your task for today is to implement
+  this system.
+ 
+ Input Description
+ None.
+ 
+ Output Description
+ 
+ Output a string signifying 50 tetromino pieces given to the player using the
+  random bag system. This will be on a single line.
+  
+ The pieces are as follows:
+ O
+ I
+ S
+ Z
+ L
+ J
+ T
+ Sample Inputs
+ 
+ None.
+ Sample Outputs
+ 
+ LJOZISTTLOSZIJOSTJZILLTZISJOOJSIZLTZISOJTLIOJLTSZO
+ OTJZSILILTZJOSOSIZTJLITZOJLSLZISTOJZTSIOJLZOSILJTS
+ ITJLZOSILJZSOTTJLOSIZIOLTZSJOLSJZITOZTLJISTLSZOIJO
+ Note
+ 
+ Although the output is semi-random, you can verify whether it is likely to be
+  correct by making sure that pieces do not repeat within chunks of seven.
+ Credit
+ 
+ This challenge was developed by /u/chunes on /rand/dailyprogrammer_ideas. If you 
+ have any challenge ideas please share them there and there's a chance we'll use 
+ them.
+ Bonus
+ 
+ Write a function that takes your output as input and verifies that it is a valid
+  sequence of pieces.
+   * @author JasonRobinson
  */
 public class TetrominoBag {
-    static char[] letters = "OISZLJT".toCharArray();
-    static int max = 50;
-    static Random r = new Random();
-    
+    static final char[] chars = {'O','I','S','Z','L','J','T'};
+    static Random rand = new Random();
+    static char[] bag;
+
     public static void main(String args[]){
-
-        //System.out.println("String of 50 tetromino characters: ");
-        char[] chars = new char[50];
+        char[] cs = new char[50];
         char prevChar = ' ';
+        bag = chars.clone();
 
-        
-        for (int i = 0; i <max;i++) {
-            chars[i] = getAChar(prevChar);
-            prevChar = chars[i];
+        for (int i = 0; i <50;i++) {
+            if(isEmpty()) bag = chars.clone();
+            cs[i] = get(prevChar);
+            prevChar = cs[i];
         }
-        System.out.println(String.valueOf(chars));
-        
-            
+        System.out.println(String.valueOf(cs));
     }
     
-    public static char getAChar(char c){
-        int Low = 0;
-        int High = 7;
-        char newChar = c;
-        int R = r.nextInt(High-Low) + Low;
-        while(letters[R] == c) {
-            R = r.nextInt(High-Low) + Low;
-            }
-        return letters[R];
+    public static char get(char c){
+        int R = rand.nextInt(7);
+        while(bag[R] == ' ') {
+            R = rand.nextInt(7);}
+        char ret = bag[R];
+        bag[R] = ' ';
+        return ret;
+    }
+
+    public static boolean isEmpty(){
+        char l = ' ';
+        for(char c: bag)
+            if (c != ' ') l = c;
+        if (l == ' ') return true;
+        else return false;
     }
 }
